5e1f7c5b2bb89ca758900824fea4dc65ea07e743
Ivan Hema <hacker19088@gmail.com>
2023-03-26 11:24:30 +0300

    Update

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,61 @@
+import { nanoid } from 'nanoid';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import { addContact } from 'redux/contactsSlice';
+import style from './ContactForm.module.css';
+
+export const ContactForm = () => {
+    const contacts = useSelector(getContacts);
+    const dispatch = useDispatch();
+  
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const { name, number } = form.elements;
+
+        const contact = {
+            id: nanoid(),
+            name: name.value,
+            number: number.value,
+        };
+
+    if (contacts.find(contact => contact.name === name.value)) {
+      Report.warning(
+        'Phonebook Warning',
+        'The contact already exists with this name',
+        'Okay',
+      );
+      return;
+        }
+    dispatch(addContact(contact));
+    form.reset();
+    };
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <label className={style.title}>Name
+                <input className={style.input}
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />
+            </label>
+            <label className={style.title}>Number
+                <input className={style.input}
+                    placeholder="000-00-00"
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                />
+            </label>
+            <button className={style.button} type="submit">
+                Add contact
+            </button>
+        </form>
+    );
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,35 @@
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { getFilter, getContacts } from 'redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
+import style from './ContactList.module.css';
+
+export const ContactList = () => {
+    const contacts = useSelector(getContacts);
+    const filters = useSelector(getFilter);
+    const dispatch = useDispatch();
+
+    const handleDelete = id => {
+        dispatch(deleteContact(id));
+    }
+    const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filters.toLowerCase())
+  );
+
+  return (
+    <>
+        {contacts.length === 0 ? (
+            <h3>Contact book is empty!</h3>) : (
+                <ul className={style.list}>
+                        {filteredContacts.map(({id, name, number}) => (
+                        <li className={style.item} key={id}>
+                            <p>
+                                {name}: {number}
+                            </p>
+                            <button className={style.button} type="button" onClick={() => handleDelete(id)} value="delete">Delete contact</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+    </>
+    )    
+}
